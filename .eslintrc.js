2464a4ea0b78828b4649113f7a923d053f2fae86
MiroslavKolosek <miroslav@kolosek.com>
2018-09-20 22:03:13 +0200

    Init commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "extends": "airbnb",
+  "rules": {
+    "no-console": 0,
+    "indent": [
+        "error",
+        2
+    ],
+    "linebreak-style": [
+        "error",
+        "unix"
+    ],
+    "quotes": [
+        "error",
+        "single"
+    ],
+    "semi": [
+        "error",
+        "never"
+    ],
+    "no-use-before-define": 0,
+    "react/prefer-stateless-function": [0, { "ignorePureComponents": true }],
+    "react/jsx-filename-extension": [0, { "extensions": [".js", ".jsx"] }]
+  }
+};
